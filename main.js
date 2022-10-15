acd409abd4e6bdf61f7c68075511ee6cd05f4ce4
Moon <114552693+bluemoonrz@users.noreply.github.com>
2022-10-14 22:50:57 -0700

    updated css & working hamburger menu
    *Added comments in CSS for better management of code
    
    *Fixed the hamburger menu and now works with navigation menu

--- main.js
+++ main.js
@@ -1,5 +1,17 @@
-const hamburger = document.querySelector('.hamburger')
+const hamburger = document.querySelector(".hamburger");
+const navbar = document.querySelector(".nav--bar");
 
-hamburger.addEventListener('click', function() {
-    hamburger.classList.toggle('active');
+hamburger.addEventListener("click", () => {
+    const visible = hamburger.getAttribute("data-visible");
+
+    if (visible === "false") {
+        hamburger.setAttribute("data-visible", true);
+
+        navbar.setAttribute("aria-expanded", true);
+    }
+    else {
+        hamburger.setAttribute("data-visible", false);
+
+        navbar.setAttribute("aria-expanded", false);
+    }
 });
